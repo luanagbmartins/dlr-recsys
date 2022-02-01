ef31145d16304a627b73f3c8dee60defe913a8fb
luanagbmartins <luanagbmartins@gmail.com>
2022-02-01 15:45:33 -0300

    add script

---
+++ movie_lens_100k_fair.sh
@@ -0,0 +1,5 @@
+python -m luigi --module train DRLTrain --dataset-version movie_lens_100k --train-version movie_lens_100k_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_100k --train-version movie_lens_100k_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_100k --train-version movie_lens_100k_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_100k --train-version movie_lens_100k_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_100k --train-version movie_lens_100k_fair --use-wandb --local-scheduler
---
+++ movie_lens_1m_fair.sh
@@ -0,0 +1,5 @@
+python -m luigi --module train DRLTrain --dataset-version movie_lens_1m --train-version movie_lens_1m_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_1m --train-version movie_lens_1m_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_1m --train-version movie_lens_1m_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_1m --train-version movie_lens_1m_fair --use-wandb --local-scheduler
+python -m luigi --module train DRLTrain --dataset-version movie_lens_1m --train-version movie_lens_1m_fair --use-wandb --local-scheduler
