70cdb3120c3ad23706ebf36141eb11e98745ecf3
luanagbmartins <luanagbmartins@gmail.com>
2022-06-24 16:11:01 -0300

    removing unused models (trivago, foursquare, ml-25m)

--- ml_100k.sh
+++
@@ -1,5 +0,0 @@
-python -m luigi --module train TrainRS --dataset-version movie_lens_100k --train-version movie_lens_100k --use-wandb --local-scheduler
-python -m luigi --module train TrainRS --dataset-version movie_lens_100k --train-version movie_lens_100k --use-wandb --local-scheduler
-python -m luigi --module train TrainRS --dataset-version movie_lens_100k --train-version movie_lens_100k --use-wandb --local-scheduler
-python -m luigi --module train TrainRS --dataset-version movie_lens_100k --train-version movie_lens_100k --use-wandb --local-scheduler
-python -m luigi --module train TrainRS --dataset-version movie_lens_100k --train-version movie_lens_100k --use-wandb --local-scheduler
